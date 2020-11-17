e400eb34dfbf466aa956e7b6b38fa23907064cfc
杨乐 <44945303+yangle-163@users.noreply.github.com>
2020-11-17 08:44:43 +0800

    05

---
+++ build/precache-manifest.e3cc70af8b21e6733c81b83748d05ac8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acebcefd90319af4063474a174a3848b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c54a51337707eb0cb87f",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "aa0a9ffa4e594693cd6d",
+    "url": "/static/css/main.baf8c220.chunk.css"
+  },
+  {
+    "revision": "c54a51337707eb0cb87f",
+    "url": "/static/js/2.5f493a60.chunk.js"
+  },
+  {
+    "revision": "932507d561d9cea5ea27440adcb27ef2",
+    "url": "/static/js/2.5f493a60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa0a9ffa4e594693cd6d",
+    "url": "/static/js/main.23586535.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
